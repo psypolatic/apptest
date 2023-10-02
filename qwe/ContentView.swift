b4f1b075f4d6ff2c209f984568963aefcfc3b367
ZXZ <xcv>
2023-10-02 10:29:54 -0400

    Initial Commit

---
+++ qwe/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  qwe
+//
+//  Created by LCHS CSIT 24 on 10/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ qwe/qweApp.swift
@@ -0,0 +1,17 @@
+//
+//  qweApp.swift
+//  qwe
+//
+//  Created by LCHS CSIT 24 on 10/2/23.
+//
+
+import SwiftUI
+
+@main
+struct qweApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
